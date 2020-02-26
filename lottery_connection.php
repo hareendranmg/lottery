e499b5189f7d75ff38d9bb4f89c10d6416ddce9d
Hareendran MG <hareendranmg@gmail.com>
2020-02-26 11:14:28 +0530

    Update lottery_connection.php

--- lottery_connection.php
+++ lottery_connection.php
@@ -4,6 +4,6 @@
 	if($conn->connect_error){
 	   die("Connection failed: " . $conn->connect_error);
         } else{
-                print_r($conn);
+//                 print_r($conn);
         }
 ?>
