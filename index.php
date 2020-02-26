64a521fa3f0cddc6a7da722931ddadc742aba3e7
Hareendran MG <hareendranmg@gmail.com>
2020-02-26 19:41:48 +0530

    get_lottery merged into index

--- index.php
+++ index.php
@@ -32,6 +32,31 @@ foreach ($nodeValues as $node) {
 	$result = $conn->query($sql);
 }
 
+// $sql = "DELETE FROM `lottery_details`
+//         ORDER BY `lottery_details_id` ASC
+//         LIMIT 10 ";
+
+// $query = $conn->query($sql);
+
+$sql = "SELECT `lottery_name`, `lottery_link`
+        FROM `lottery_details`
+        LIMIT 10 ";
+
+$query = $conn->query($sql);
+$i = 1;
+
+$lottery_details = [];
+
+while ($row = $query->fetch_assoc()) {
+    array_push($lottery_details, [
+        'No' => $i,
+        'lottery_name' => $row['lottery_name'],
+        'lottery_link' => $row['lottery_link'],
+    ]);
+    $i++;
+}
+
+echo (json_encode($lottery_details));
 
 
 
